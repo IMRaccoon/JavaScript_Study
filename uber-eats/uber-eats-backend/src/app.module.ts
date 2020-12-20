7aa5708598d9979ecc624f80f5963487831b9c32
DoHyeon Jeon <whdies816@gmail.com>
2020-12-20 16:59:14 +0900

    DB config

--- uber-eats/uber-eats-backend/src/app.module.ts
+++ uber-eats/uber-eats-backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: true }), RestaurantsModule],
+  imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'jeondohyeon',
+      password: '1234',
+      database: 'uber-eats',
+      synchronize: true,
+      logging: true,
+    }),
+    GraphQLModule.forRoot({ autoSchemaFile: true }),
+  ],
   controllers: [],
   providers: [],
 })
