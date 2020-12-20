c25ab9b389851f5de9ae229ebf92a33ef12fc373
DoHyeon Jeon <whdies816@gmail.com>
2020-12-20 17:43:18 +0900

    TypeORM Data Mapping Pattern

--- uber-eats/uber-eats-backend/src/restaurants/restaurants.module.ts
+++ uber-eats/uber-eats-backend/src/restaurants/restaurants.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
-import { RestaurantService } from './restaurants.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
+import { RestuarantsResolver } from './restaurants.resolver';
+import { RestaurantsService } from './restaurants.service';
 
 @Module({
-  providers: [RestaurantService],
+  imports: [TypeOrmModule.forFeature([Restaurant])],
+  providers: [RestuarantsResolver, RestaurantsService],
 })
 export class RestaurantsModule {}
--- uber-eats/uber-eats-backend/src/restaurants/restaurants.resolver.ts
+++ uber-eats/uber-eats-backend/src/restaurants/restaurants.resolver.ts
@@ -1,13 +1,15 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dto/create-restuarant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantsService } from './restaurants.service';
 
 @Resolver(() => Restaurant)
-export class RestaurantService {
+export class RestuarantsResolver {
+  constructor(private readonly restaurantsService: RestaurantsService) {}
+
   @Query(() => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log(veganOnly);
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantsService.getAll();
   }
 
   @Mutation(() => Boolean)
