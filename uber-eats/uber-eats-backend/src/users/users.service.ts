68a7ba84229cc18c0360b0995b68b79ffe721a06
DoHyeon Jeon <whdies816@gmail.com>
2020-12-21 21:58:52 +0900

    user verification schema

--- uber-eats/uber-eats-backend/src/users/users.service.ts
+++ uber-eats/uber-eats-backend/src/users/users.service.ts
@@ -6,11 +6,14 @@ import { CreateAccountInput } from './dtos/create-account.dto';
 import { EditProfileInput } from './dtos/edit-profile.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    @InjectRepository(Verification)
+    private readonly verification: Repository<Verification>,
     private readonly jwtService: JwtService,
   ) {}
 
@@ -24,7 +27,12 @@ export class UsersService {
       if (exists) {
         return { ok: false, error: 'There is a user with that email already' };
       }
-      await this.users.save(this.users.create({ email, password, role }));
+      const user = await this.users.save(
+        this.users.create({ email, password, role }),
+      );
+      await this.verification.save(
+        this.verification.create({ code: '', user }),
+      );
       return { ok: true };
     } catch (e) {
       return { ok: false, error: "Couldn't create account" };
@@ -62,6 +70,8 @@ export class UsersService {
     const user = await this.users.findOne({ id: userId });
     if (email) {
       user.email = email;
+      user.verified = false;
+      await this.verification.save(this.verification.create({ user }));
     }
     if (password) {
       user.password = password;
