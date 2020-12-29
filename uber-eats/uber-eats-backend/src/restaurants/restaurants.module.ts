afbb4e4eb7af6366f1695733c195903538075950
DoHyeon Jeon <whdies816@gmail.com>
2020-12-29 23:51:17 +0900

    Dish Entity

--- uber-eats/uber-eats-backend/src/restaurants/restaurants.module.ts
+++ uber-eats/uber-eats-backend/src/restaurants/restaurants.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dish } from './entities/dish.entity';
 import { CategoryRepository } from './repositories/category.repository';
 import { RestaurantRepository } from './repositories/restaurant.repository';
-import { CategoryResolver, RestuarantsResolver } from './restaurants.resolver';
+import {
+  CategoryResolver,
+  DishResolver,
+  RestuarantsResolver,
+} from './restaurants.resolver';
 import { RestaurantsService } from './restaurants.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([RestaurantRepository, CategoryRepository]),
+    TypeOrmModule.forFeature([RestaurantRepository, CategoryRepository, Dish]),
   ],
-  providers: [RestuarantsResolver, RestaurantsService, CategoryResolver],
+  providers: [
+    RestuarantsResolver,
+    RestaurantsService,
+    CategoryResolver,
+    DishResolver,
+  ],
 })
 export class RestaurantsModule {}
