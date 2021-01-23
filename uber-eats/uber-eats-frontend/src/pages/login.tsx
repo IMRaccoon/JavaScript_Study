2188429c791a6ca80102262dbd73aa265abee544
DoHyeon Jeon <whdies816@gmail.com>
2021-01-23 17:57:36 +0900

    Login Mutation

--- uber-eats/uber-eats-frontend/src/pages/login.tsx
+++ uber-eats/uber-eats-frontend/src/pages/login.tsx
@@ -1,15 +1,44 @@
-import { useForm } from 'react-hook-form';
+import { gql, useMutation } from "@apollo/client";
+import { useForm } from "react-hook-form";
+import { FormError } from "../components/form-error";
+import {
+  loginMutation,
+  loginMutationVariables,
+} from "../__generated__/loginMutation";
+
+const LOGIN_MUTATION = gql`
+  mutation loginMutation($loginInput: LoginInput!) {
+    login(input: $loginInput) {
+      ok
+      error
+      token
+    }
+  }
+`;
 
 interface ILoginForm {
-  email?: string;
-  password?: string;
+  email: string;
+  password: string;
 }
 
 export const Login = () => {
   const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
+  const onCompleted = ({ login: { ok, token, error } }: loginMutation) => {
+    if (ok) {
+      console.log(token);
+    }
+  };
+
+  const [loginMutation, { data: loginMutationResult, loading }] = useMutation<
+    loginMutation,
+    loginMutationVariables
+  >(LOGIN_MUTATION, { onCompleted });
 
   const onSubmit = () => {
-    console.log(getValues());
+    if (!loading) {
+      const { email, password } = getValues();
+      loginMutation({ variables: { loginInput: { email, password } } });
+    }
   };
 
   return (
@@ -21,7 +50,7 @@ export const Login = () => {
           className="grid gap-3 mt-5 px-5"
         >
           <input
-            ref={register({ required: 'Email is required' })}
+            ref={register({ required: "Email is required" })}
             type="email"
             name="email"
             required
@@ -29,12 +58,10 @@ export const Login = () => {
             className="input"
           />
           {errors.email?.message && (
-            <span className="font-medium text-red-500">
-              {errors.email?.message}
-            </span>
+            <FormError errorMessage={errors.email?.message} />
           )}
           <input
-            ref={register({ required: 'Password is required', minLength: 10 })}
+            ref={register({ required: "Password is required" })}
             type="password"
             name="password"
             required
@@ -42,16 +69,17 @@ export const Login = () => {
             className="input"
           />
           {errors.password?.message && (
-            <span className="font-medium text-red-500">
-              {errors.password?.message}
-            </span>
+            <FormError errorMessage={errors.password?.message} />
           )}
-          {errors.password?.type === 'minLength' && (
-            <span className="font-medium text-red-500">
-              Password must be for than 10 chars.
-            </span>
+          {errors.password?.type === "minLength" && (
+            <FormError errorMessage="Password must be for than 10 chars." />
           )}
-          <button className="button mt-3">Log In</button>
+          <button className="button mt-3">
+            {loading ? "Loading..." : "Log In"}
+          </button>
+          {loginMutationResult?.login?.error && (
+            <FormError errorMessage={loginMutationResult?.login?.error} />
+          )}
         </form>
       </div>
     </div>
