ae733aae851d60024e7ed74e388b462daabd7b13
DoHyeon Jeon <whdies816@gmail.com>
2021-01-27 23:40:12 +0900

    cypress Login Page

---
+++ uber-eats/uber-eats-frontend/cypress/integration/first-test.ts
@@ -0,0 +1,33 @@
+describe('Login Page', () => {
+  const user = cy;
+  it('should see login page', () => {
+    user.visit('/').title().should('eq', 'Login | Uber Eats');
+  });
+
+  it('can see email / password validation error', () => {
+    user.visit('/');
+    user.findByPlaceholderText(/email/i).type('valid@email');
+    user.findByRole(/alert/i).should('have.text', 'Please enter a valid email');
+    user.findByPlaceholderText(/email/i).clear();
+    user.findByRole(/alert/i).should('have.text', 'Email is required');
+    user.findByPlaceholderText(/email/i).type('test@test.com');
+    user
+      .findByPlaceholderText(/password/i)
+      .type('test')
+      .clear();
+    user.findByRole(/alert/i).should('have.text', 'Password is required');
+  });
+
+  it('can fill out the form', () => {
+    user
+      .visit('/')
+      .get('[type="email"]')
+      .type('test1@test.com')
+      .get('[name="password"]')
+      .type('test')
+      .get('.text-lg')
+      .should('not.have.class', 'pointer-events-none')
+      .click();
+    user.window().its('localStorage.uber-token').should('be.a', 'string');
+  });
+});
