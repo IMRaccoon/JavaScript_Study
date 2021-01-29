767ceda2049cef640643d1f0434ab23c82f8118c
DoHyeon Jeon <whdies816@gmail.com>
2021-01-30 00:32:34 +0900

    Add Restaurant Base

--- uber-eats/uber-eats-frontend/src/pages/owner/my-restaurant.tsx
+++ uber-eats/uber-eats-frontend/src/pages/owner/my-restaurant.tsx
@@ -1,5 +1,42 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
+import { RESTAURANT_FRAGMENT } from "../../fragments";
+import { myRestaurants } from "../../__generated__/myRestaurants";
+
+const MY_RESTAUTANTS_QUERY = gql`
+  query myRestaurants {
+    myRestaurants {
+      ok
+      error
+      restaurants {
+        ...RestaurantParts
+      }
+    }
+  }
+  ${RESTAURANT_FRAGMENT}
+`;
 
 export const MyRestaurants = () => {
-  return <h1>My Restaurant</h1>;
+  const { data } = useQuery<myRestaurants>(MY_RESTAUTANTS_QUERY);
+
+  return (
+    <div>
+      <Helmet>
+        <title>My Restaurants | Uber Eats</title>
+      </Helmet>
+      <div className="container mt-32">
+        <h2 className="text-4xl font-medium ub-10">My Restaurants</h2>
+        {data?.myRestaurants.ok && data.myRestaurants.restaurants.length === 0 && (
+          <>
+            <h4 className="text-xl mb-5">You have no restaurants</h4>
+            <Link className="link" to="/add-restaurant">
+              Create one &rarr;
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
